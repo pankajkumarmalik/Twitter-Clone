3d69c1f27cf17c5f7659df441049b636ae354c26
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2024-04-30 15:38:25 +0530

    1- Creating Auth Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//middlewears
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
