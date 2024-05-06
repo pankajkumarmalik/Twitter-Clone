270c6bbdef18892efba7a70dedaefc8fddf0b2c6
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2024-05-06 20:40:27 +0530

    3- Notification query and mutation added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -4,10 +4,12 @@ export const getNotifications = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const notifications = await Notification.find({ to: userId }).populate({
-      path: "from",
-      select: "username profileImg",
-    });
+    const notifications = await Notification.find({ to: userId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "from",
+        select: "username profileImg",
+      });
 
     await Notification.updateMany({ to: userId }, { read: true });
 
