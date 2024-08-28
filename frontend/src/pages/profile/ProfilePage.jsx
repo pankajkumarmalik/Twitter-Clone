7df84ef35b44859e2360164908397a10951f5e02
Pankaj Kumar Malik <140264854+pankajkumarmalik@users.noreply.github.com>
2024-08-28 18:12:39 +0530

    Update ProfilePage.jsx

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -95,9 +95,9 @@ const ProfilePage = () => {
                 </Link>
                 <div className="flex flex-col">
                   <p className="font-bold text-lg">{user?.fullName}</p>
-                  <span className="text-sm text-slate-500">
+{/*                   <span className="text-sm text-slate-500">
                     {POSTS?.length} posts
-                  </span>
+                  </span> */}
                 </div>
               </div>
               {/* COVER IMG */}
